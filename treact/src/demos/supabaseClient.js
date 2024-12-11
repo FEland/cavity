5e2822b39f1ed57e924682fffd3652f53fdb8f46
Effie L <Effieland@aol.com>
2024-12-11 10:06:45 +0200

    ok

--- treact/src/demos/supabaseClient.js
+++ treact/src/demos/supabaseClient.js
@@ -1,7 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = process.env.REACT_APP_supabaseUrl;
-// const supabaseUrl = 'https://pmdubdmaqrdawtogjxon.supabase.co';
 const supabaseKey = process.env.REACT_APP_supabaseKey;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
